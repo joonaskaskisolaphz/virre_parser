40f4e84a97139da288cf61b58264d2155ca371bc
Administrator <admin@example.com>
2015-09-19 00:38:38 +0200

    $x -> $virre @ run_virreParser.php

--- run_virreParser.php
+++ run_virreParser.php
@@ -3,14 +3,14 @@
 require 'virreParser.php';
 require 'class.phpmailer.php';
 
-$x = new virreParser();
+$virre = new virreParser();
 
 $given_arguments = $argv;
 array_shift($given_arguments);
 
 if ( ! empty($given_arguments)) {
 	foreach ($given_arguments as $given_argument) {
-		$x->getCompanysData($given_argument);
+		$virre->getCompanysData($given_argument);
 	}
 }
 else
